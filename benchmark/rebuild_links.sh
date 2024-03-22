15b5d64caab0e45ff4a7da912e29d6f6e7ee8c16
Labrak Yanis <yanis.labrak@univ-avignon.fr>
2024-03-22 23:14:42 +0100

    Add files via upload

---
+++ benchmark/rebuild_links.sh
@@ -0,0 +1,27 @@
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/cas/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/clister/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/deft2019/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/deft2020/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/deft2021/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/diamed/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/e3c/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/essai/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/frenchmedmcqa/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/mantragsc/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/morfitt/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/pxcorpus/scripts/utils.py
+rm /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/quaero/scripts/utils.py
+
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/cas/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/clister/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/deft2019/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/deft2020/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/deft2021/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/diamed/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/e3c/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/essai/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/frenchmedmcqa/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/mantragsc/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/morfitt/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/pxcorpus/scripts/utils.py
+ln -s /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/utils.py /users/ylabrak/TokenizationDrBERT/drbenchmark_v3/recipes/quaero/scripts/utils.py
